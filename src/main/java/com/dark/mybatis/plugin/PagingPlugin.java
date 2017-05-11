a65a7b3a229b9ae8f54835ff6ea4275ac7903685
dark <xt00002003@163.com>
2017-05-11 18:57:47 +0800

    添加mybatis 代码

--- src/main/java/com/dark/mybatis/plugin/PagingPlugin.java
+++ src/main/java/com/dark/mybatis/plugin/PagingPlugin.java
@@ -1,13 +1,23 @@
 package com.dark.mybatis.plugin;
 
+import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.*;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
+import org.apache.ibatis.scripting.defaults.DefaultParameterHandler;
+import org.apache.ibatis.session.Configuration;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 /**
  * java_demo
@@ -46,7 +56,130 @@ public class PagingPlugin implements Interceptor {
         }
         BoundSql boundSql=(BoundSql)metaObject.getValue("delegate.boundSql");
         Object parameterObject=boundSql.getParameterObject();
-        return null;
+        PageParams pageParams=getPageParams(parameterObject);
+        if (pageParams==null){
+            return invocation.proceed();
+        }
+
+        Integer pageNum=pageParams.getPage()==null?this.defaultPage:pageParams.getPage();
+        Integer pageSize=pageParams.getPageSize()==null?this.defaultPageSize:pageParams.getPageSize();
+        Boolean useFlag=pageParams.getUseFlag()==null?this.defaultUseFlag:pageParams.getUseFlag();
+        Boolean checkFlag=pageParams.getCheckFlag()==null?this.defaultCheckFlag:pageParams.getCheckFlag();
+        if(!useFlag){
+            return invocation.proceed();
+        }
+
+        int total=getTotal(invocation,metaObject,boundSql);
+        setTotalToPageParams(pageParams,total,pageSize);
+
+        checkPage(checkFlag,pageNum,pageParams.getTotalPage());
+
+
+        return changeSQL(invocation,metaObject,boundSql,pageNum,pageSize);
+    }
+
+    private StatementHandler getUnProxyObject(Invocation invocation){
+        StatementHandler statementHandler=(StatementHandler)invocation.getTarget();
+        MetaObject metaObject= SystemMetaObject.forObject(statementHandler);
+        Object obj=null;
+        while (metaObject.hasGetter("h")){
+            obj=metaObject.getValue("h");
+        }
+        if (obj==null){
+            return statementHandler;
+        }
+
+        return (StatementHandler)obj;
+
+    }
+
+    private Boolean checkSelect(String sql){
+        String trimSql=sql.trim();
+        int index=trimSql.toLowerCase().indexOf("select");
+        return index==0;
+    }
+
+    private PageParams getPageParams(Object parameterObject){
+        if (parameterObject==null){
+            return null;
+        }
+        PageParams pageParams=null;
+        if (parameterObject instanceof Map){
+            Map<String,Object> paramMap=(Map<String,Object>)parameterObject;
+            Set<String> keySet=paramMap.keySet();
+            Iterator<String> iterator=keySet.iterator();
+            while (iterator.hasNext()){
+                String key=iterator.next();
+                Object value=paramMap.get(key);
+                if (value instanceof PageParams){
+                    return (PageParams) value;
+                }
+            }
+        }else if(parameterObject instanceof PageParams){
+            pageParams=(PageParams)parameterObject;
+        }
+        return pageParams;
+    }
+
+
+    private Integer getTotal(Invocation invocation,MetaObject metaObject,BoundSql boundSql){
+        MappedStatement mappedStatement=(MappedStatement)metaObject.getValue("delegate.mappedStatement");
+        Configuration configuration=mappedStatement.getConfiguration();
+        String sql=(String) metaObject.getValue("delegate.boundSql.sql");
+        String countSql="select count(*) as total from ("+sql+") $_paging";
+        Connection connection=(Connection) invocation.getArgs()[0];
+        PreparedStatement ps=null;
+        int total=0;
+        try {
+            ps=connection.prepareStatement(countSql);
+            BoundSql countBoundSql=new BoundSql(configuration,countSql,boundSql.getParameterMappings(),boundSql.getParameterObject());
+            ParameterHandler parameterHandler=new DefaultParameterHandler(mappedStatement,boundSql.getParameterObject(),countBoundSql);
+            parameterHandler.setParameters(ps);
+            ResultSet rs=ps.executeQuery();
+            while (rs.next()){
+                total=rs.getInt("total");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (ps!=null){
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        return total;
+
+    }
+
+    private void checkPage(Boolean checkFlag,Integer pageNum,Integer pageTotal) throws Exception{
+        if (checkFlag){
+            if (pageNum>pageTotal){
+                throw new Exception("select error! pageNum>pageTotal");
+            }
+        }
+    }
+
+    private void setTotalToPageParams(PageParams pageParams,Integer total,Integer pageSize){
+        pageParams.setTotal(total);
+        Integer totalPage=total%pageSize==0?total/pageSize:total/pageSize+1;
+        pageParams.setTotalPage(totalPage);
+    }
+
+    private Object changeSQL(Invocation invocation,MetaObject metaObject,BoundSql boundSql,Integer page,Integer pageSize)throws Exception{
+        String sql=(String) metaObject.getValue("delegate.boundSql.sql");
+        String newSql="select *  from ( "+sql+" ) $_paging_table limit ? , ? ";
+        metaObject.setValue("delegate.boundSql.sql",newSql);
+        PreparedStatement ps=(PreparedStatement)invocation.proceed();
+        int count=ps.getParameterMetaData().getParameterCount();
+        ps.setInt(count-1,(page-1)*pageSize);
+        ps.setInt(count,pageSize);
+        return ps;
+
     }
 
     @Override
